bd3fa5e282d7e019b380fde734d0a8273c3fefea
Balo <balobatino@gmail.com>
2026-01-21 09:50:29 +0400

    - gameMode page auto refresh difficult status, no need to call from winLevelPage

--- DemoCocos2415/assets/Script/MainGame/UI/WinLevelPage.ts
+++ DemoCocos2415/assets/Script/MainGame/UI/WinLevelPage.ts
@@ -110,11 +110,6 @@ export default class WinLevelPage extends Singleton<WinLevelPage> {
      */
     private onGameModeSelectClicked(): void {
         this.navigateToPage(() => GameModePage.getInstance<GameModePage>());
-        // also call it to update content since user score may changed
-        const gameModePage = GameModePage.getInstance<GameModePage>();
-        if (gameModePage) {
-            gameModePage.configContentForDifficultMode();
-        }
     }
 
     /**
